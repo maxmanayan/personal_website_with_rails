710657127dcea73a7f1a494d2e2129ef14b95a47
Max Manayan <maximillian521@gmail.com>
2021-05-04 14:31:08 -0600

    re-styled Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,34 +10,34 @@ const Home = () => {
   return(
     <div style={{ height: '90vh', padding: '1em'}}>
       <Container >
-        <Row style={{paddingTop: '2em', paddingLeft: '1em'}}>
+        <Row style={{paddingTop: '2em'}}>
           <Col xs={12} md={{span: 6, offset: 5}}>
-            <h1 style={{color:`${MAX_BLUE}`, fontSize: '10em', fontWeight: 'bold'}}>Max Manayan</h1>
+            <h1 style={{color:`${MAX_BLUE}`, fontSize: '10vw', fontWeight: 'bold'}}>Max Manayan</h1>
           </Col>
         </Row>
 
-        <Row style={{paddingBottom: '2em', paddingLeft: '2em'}}>
+        <Row style={{paddingBottom: '2em'}}>
           <Col  xs={12} md={{span: 6, offset: 5}}>
-            <h2 style={{color:`${MAX_DARK}`, fontSize: '3em'}}>Software Engineer</h2>
+            <h2 style={{color:`${MAX_DARK}`, fontSize: '3vw'}}>Software Engineer</h2>
           </Col>
         </Row>
 
-        <Row style={{display: 'flex', justifyContent: 'center'}}>
+        <Row style={{display: 'flex', justifyContent: 'flex-start'}}>
           <Col  xs={12} md={{span: 6, offset: 5}}>
             <Row>
               <Col xs={12} md={{span: 4}} style={{display: 'flex', justifyContent: 'center'}}>
                 <Link to="/portfolio">
-                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em 3em', fontWeight: 'bold'}}>Portfolio</Button>
+                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em', width: '10em', margin: '1em', fontWeight: 'bold'}}>Portfolio</Button>
                 </Link>
               </Col>
               <Col xs={12} md={{span: 4}} style={{display: 'flex', justifyContent: 'center'}} >
                 <Link to="/about">
-                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em 3em', fontWeight: 'bold'}}>About</Button>
+                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em', width: '10em', margin: '1em', fontWeight: 'bold'}}>About</Button>
                 </Link>
               </Col>
               <Col xs={12} md={{span: 4}} style={{display: 'flex', justifyContent: 'center'}} >
                 <Link to="/connect">
-                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em 3em', fontWeight: 'bold'}}>Connect</Button>
+                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em', width: '10em', margin: '1em', fontWeight: 'bold'}}>Connect</Button>
                 </Link>
               </Col>
             </Row>
