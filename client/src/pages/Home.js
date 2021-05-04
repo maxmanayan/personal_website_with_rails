c33bff25787980378a945fd48c5ca7c61b1f41cc
Max Manayan <maximillian521@gmail.com>
2021-05-04 14:02:33 -0600

    added styled components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,8 @@
 import { Button, Col, Container, Row } from "react-bootstrap"
 import { Link } from "react-router-dom"
+
 import { CONTENT_BACKGROUND_GRAY, MAX_BLUE, MAX_DARK, MAX_WHITE } from "../colors"
+import { HOMEPAGE_BUTTON } from "../styles"
 
 
 
@@ -14,28 +16,28 @@ const Home = () => {
           </Col>
         </Row>
 
-        <Row style={{paddingBottom: '3em', paddingLeft: '1em'}}>
+        <Row style={{paddingBottom: '2em', paddingLeft: '2em'}}>
           <Col  xs={12} md={{span: 6, offset: 5}}>
             <h2 style={{color:`${MAX_DARK}`, fontSize: '3em'}}>Software Engineer</h2>
           </Col>
         </Row>
 
-        <Row>
+        <Row style={{display: 'flex', justifyContent: 'center'}}>
           <Col  xs={12} md={{span: 6, offset: 5}}>
             <Row>
               <Col xs={12} md={{span: 4}} style={{display: 'flex', justifyContent: 'center'}}>
                 <Link to="/portfolio">
-                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`}}>Portfolio</Button>
+                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em 3em'}}>Portfolio</Button>
                 </Link>
               </Col>
               <Col xs={12} md={{span: 4}} style={{display: 'flex', justifyContent: 'center'}} >
                 <Link to="/about">
-                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`}}>About</Button>
+                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em 3em'}}>About</Button>
                 </Link>
               </Col>
               <Col xs={12} md={{span: 4}} style={{display: 'flex', justifyContent: 'center'}} >
                 <Link to="/connect">
-                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`}}>Connect</Button>
+                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em 3em'}}>Connect</Button>
                 </Link>
               </Col>
             </Row>
---
+++ client/src/styles.js
@@ -0,0 +1,10 @@
+
+import { Button } from "react-bootstrap";
+import styled from "styled-components";
+
+export const HOMEPAGE_BUTTON = styled(Button)`
+  background: #628EAC;
+  border-color: #628EAC;
+  color: #EFEFEF;
+  padding: 1em 4em;
+`
