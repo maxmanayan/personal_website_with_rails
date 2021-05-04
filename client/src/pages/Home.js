ddd55b0a035f02953528d82a46e3dbebab53815f
Max Manayan <maximillian521@gmail.com>
2021-05-04 14:16:41 -0600

    added lato font

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
       <Container >
         <Row style={{paddingTop: '2em', paddingLeft: '1em'}}>
           <Col xs={12} md={{span: 6, offset: 5}}>
-            <h1 style={{color:`${MAX_BLUE}`, fontSize: '10em'}}>Max Manayan</h1>
+            <h1 style={{color:`${MAX_BLUE}`, fontSize: '10em', fontWeight: 'bold'}}>Max Manayan</h1>
           </Col>
         </Row>
 
@@ -27,17 +27,17 @@ const Home = () => {
             <Row>
               <Col xs={12} md={{span: 4}} style={{display: 'flex', justifyContent: 'center'}}>
                 <Link to="/portfolio">
-                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em 3em'}}>Portfolio</Button>
+                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em 3em', fontWeight: 'bold'}}>Portfolio</Button>
                 </Link>
               </Col>
               <Col xs={12} md={{span: 4}} style={{display: 'flex', justifyContent: 'center'}} >
                 <Link to="/about">
-                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em 3em'}}>About</Button>
+                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em 3em', fontWeight: 'bold'}}>About</Button>
                 </Link>
               </Col>
               <Col xs={12} md={{span: 4}} style={{display: 'flex', justifyContent: 'center'}} >
                 <Link to="/connect">
-                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em 3em'}}>Connect</Button>
+                  <Button style={{background:`${MAX_BLUE}`, borderColor:`${MAX_BLUE}`, padding: '1em 3em', fontWeight: 'bold'}}>Connect</Button>
                 </Link>
               </Col>
             </Row>
