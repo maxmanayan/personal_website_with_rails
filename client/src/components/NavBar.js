6f5e7e5bcdb581d4373dea72e3a42ccaa6403937
Max Manayan <maximillian521@gmail.com>
2021-05-04 22:18:32 -0600

    cleaned up code

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,10 +4,10 @@ import { MAX_BLUE, MAX_DARK, MAX_WHITE } from "../colors"
 const NavBar = () => {
   return(
     <>
-      <Navbar sticky="top" collapseOnSelect expand="lg" style={{background: `${MAX_DARK}`}}>
+      <Navbar sticky="top" collapseOnSelect expand="lg" variant='dark' style={{background: `${MAX_DARK}`}}>
         <Navbar.Brand href="/" style={{color: `${MAX_BLUE}`, fontWeight: 'bold'}}>Home</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav" style={{color: `${MAX_WHITE}`}}>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"  style={{color: `${MAX_WHITE}`}} />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
           </Nav>
           <Nav>
